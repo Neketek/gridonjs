663446514b4472bcf6afa2f0749cf6cd92415dcc
neketek <neketek@gmail.com>
2016-08-11 02:40:08 +0300

    Small working example

--- examples/1/main.js
+++ examples/1/main.js
@@ -1,13 +1,18 @@
 var GridOnJs = window.GridOnJs;
 GridOnJs.autodiscover();
-layout32x18 = [//16x9 * 2
+var layout32x18 = [//16x9 * 2
   ["title",1,1,30,2],
   ["content",1,3,19,12],
   ["login",20,3,11,12],
   ["footer",1,15,30,2]
 ];
 
+var loginLayout10x10 = [
+  ["login-button",1,1,8,8]
+];
+
 GridOnJs.get("body").transformOneByParamList(["main",0,0,10,10]);
 GridOnJs.get("main").rows = 18;
 GridOnJs.get("main").cols = 32;
 GridOnJs.get("main").transformManyByParamLists(layout32x18);
+GridOnJs.get("login").transformManyByParamLists(loginLayout10x10);
